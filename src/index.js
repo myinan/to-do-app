8d3a44711fbd11d98df87082b78da3c7281e9521
myinan <m.yasir.inan@gmail.com>
2023-12-18 01:25:40 +0300

    Create function to render the task arrays to main

--- src/index.js
+++ src/index.js
@@ -7,7 +7,22 @@ import { allTasks, todayTasks, nextSevenDaysTasks, importantTasks } from './modu
 const newTodo = new Todo("Go shopping", "You need to buy new shoes.", "18/12/2023", "important");
 events.emit("todoAdded", newTodo);
 
-console.log(allTasks);
-console.log(todayTasks);
-console.log(nextSevenDaysTasks);
-console.log(importantTasks);
+const main = document.getElementsByClassName("main-container");
+const sideBarContainer = document.getElementById("sidebar-container");
+
+sideBarContainer.addEventListener("click", renderTasks);
+
+const tasksArrays = {
+    allTasks,
+    todayTasks,
+    nextSevenDaysTasks,
+    importantTasks,
+};
+
+function renderTasks(event) {
+    const clickedId = event.target.id;
+
+    if (Object.keys(tasksArrays).includes(clickedId)) {
+        console.log(tasksArrays[clickedId]);
+    }
+}
