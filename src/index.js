f45b89c70489274ae90b2ec773c323ba0cd6ac65
myinan <m.yasir.inan@gmail.com>
2023-12-18 23:53:00 +0300

    Add the functionality to dynamically create projects

--- src/index.js
+++ src/index.js
@@ -5,36 +5,12 @@ import { renderOnClick } from './modules/renderToMain.js';
 import Project, { renderProject } from "./modules/projectsModule.js";
 import { addProjectOnClick } from './modules/createProjectFunc.js';
 
-addProjectOnClick();
-
-/* const newProject = new Project("initialProject");
-const newProject2 = new Project("initialProject2");
-
-// Render the project item on the side container
-renderProject(newProject);
-renderProject(newProject2);
-
-//Create a todo item
-newProject.createTodo("Go shopping", "You need to buy new pants.");
-newProject.createTodo("Go fishing", "You need to buy new bait.");
-newProject.createTodo("Go jogging", "You need to exercise.", "20/12/2023");
-newProject.createTodo("Eat dinner", "", "18/12/2023");
-
-newProject2.createTodo("Go shopping", "You need to buy new pants.");
-newProject2.createTodo("Go fishing", "You need to buy new bait.");
-newProject2.createTodo("Go jogging", "You need to exercise.", "20/12/2023");
-newProject2.createTodo("Eat dinner", "", "18/12/2023");
- */
-// Render clicked todo items to the main container
-renderOnClick();
-
-const tasksArrays = {
+export const tasksArrays = {
     allTasks,
     todayTasks,
     nextSevenDaysTasks,
     importantTasks,
 };
 
-/* tasksArrays[newProject.id] = newProject.todoArr;
-tasksArrays[newProject2.id] = newProject2.todoArr; */
-export { tasksArrays };
+addProjectOnClick();
+renderOnClick();
--- src/modules/createProjectFunc.js
+++ src/modules/createProjectFunc.js
@@ -1,13 +1,31 @@
+import Project, { renderProject } from "./projectsModule.js";
+import { tasksArrays } from "../index.js";
+
 const projectsInnerContainer = document.querySelector("#projects-inner-container");
 const createProjectButton = document.querySelector(".project-add-button");
 const form = document.querySelector("#project-form");
+const input = document.querySelector("#project-input");
 
-function createProject() {
+function removeHidden() {
     form.classList.remove("hidden");
 }
 
-export function addProjectOnClick() {
-    createProjectButton.addEventListener("click", createProject);
+function handleClick(event) {
+    if (event.target.value == "Add") {
+        event.preventDefault();
+        const newProject = new Project(`${input.value}`);
+        renderProject(newProject);
+        form.classList.add("hidden");
+
+        newProject.createTodo("Go shopping", "You need to buy new pants.");
+        tasksArrays[newProject.id] = newProject.todoArr;
+    }
+    else if (event.target.value == "Cancel") {
+        form.classList.add("hidden");
+    }
 }
 
-
+export function addProjectOnClick() {
+    createProjectButton.addEventListener("click", removeHidden);
+    form.addEventListener("click", handleClick);
+}
