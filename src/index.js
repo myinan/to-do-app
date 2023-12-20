695001fe69ec7d6d4d4328aefe40d0e13ae79311
myinan <m.yasir.inan@gmail.com>
2023-12-20 21:57:58 +0300

    Complete todo creation functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
-import { events } from "./modules/events.js";
 import { allTasks, todayTasks, nextSevenDaysTasks, importantTasks } from './modules/homeModule.js';
 import { renderOnClick } from './modules/renderToMain.js';
-import Project, { renderProject } from "./modules/projectsModule.js";
 import { addProjectOnClick } from './modules/createProjectFunc.js';
 import { addTaskBtnClicked } from './modules/createTodoFunc.js';
 
