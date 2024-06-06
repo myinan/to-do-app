2fe46e74b758e6a08a7e91f6631b1b7ca8814915
myinan <m.yasir.inan@gmail.com>
2024-06-06 03:33:19 +0300

    Add MiniCssExtractPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,30 @@
-const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
- module.exports = merge(common, {
-   mode: 'production',
-   devtool: 'source-map',
-   module: {
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
- });
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+    }),
+  ],
+});
